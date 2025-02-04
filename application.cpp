a10a1c4864d672f1df6ab2c33166925fba4c4dbb
abhishta gatya <565105@mail.muni.cz>
2025-02-05 00:12:43 +0100

    Initial Commit

---
+++ application.cpp
@@ -0,0 +1,223 @@
+#include "application.hpp"
+#include "model_ubo.hpp"
+#include "utils.hpp"
+#include <random>
+
+Application::Application(int initial_width, int initial_height, std::vector<std::string> arguments)
+	: PV227Application(initial_width, initial_height, arguments) {
+	Application::compile_shaders();
+	prepare_cameras();
+	prepare_materials();
+	prepare_textures();
+	prepare_lights();
+	prepare_scene();
+	prepare_framebuffers();
+}
+
+Application::~Application() {}
+
+// Shaders
+void Application::compile_shaders() {
+	default_unlit_program = ShaderProgram(lecture_shaders_path / "object.vert", lecture_shaders_path / "unlit.frag");
+	default_lit_program = ShaderProgram(lecture_shaders_path / "object.vert", lecture_shaders_path / "lit.frag");
+
+	star_particle_program = ShaderProgram();
+	star_particle_program.add_vertex_shader(lecture_shaders_path / "star_particle.vert");
+	star_particle_program.add_fragment_shader(lecture_shaders_path / "star_particle.frag");
+	star_particle_program.add_geometry_shader(lecture_shaders_path / "star_particle.geom");
+	star_particle_program.link();
+
+	std::cout << "Shaders are reloaded." << std::endl;
+}
+
+// Initialize Scene
+void Application::prepare_cameras() {
+	// Sets the default camera position.
+	camera.set_eye_position(glm::radians(-45.f), glm::radians(20.f), 25.f);
+	// Computes the projection matrix.
+	camera_ubo.set_projection(
+		glm::perspective(glm::radians(45.f), static_cast<float>(this->width) / static_cast<float>(this->height), 1.0f, 1000.0f));
+	camera_ubo.update_opengl_data();
+}
+
+// Materials
+void Application::prepare_materials() {}
+
+// Textures
+void Application::prepare_textures() {
+	star_tex = TextureUtils::load_texture_2d(lecture_textures_path / "star.png");
+	// Particles are really small, use mipmaps for them.
+	TextureUtils::set_texture_2d_parameters(star_tex, GL_REPEAT, GL_REPEAT, GL_LINEAR_MIPMAP_LINEAR, GL_LINEAR);
+}
+
+// Lights
+void Application::prepare_lights() {}
+
+// Scenes
+void Application::prepare_scene() {
+	// Initializes the particles.
+	particles.resize(max_particle_count); // Resize the vector to the maximum number of particles.
+	
+	// Initializes the particle buffer.
+	glGenBuffers(1, &particle_buffer);
+	reset_particles();
+}
+
+// Framebuffers
+void Application::prepare_framebuffers() {}
+
+// On Resize
+void Application::resize_fullscreen_textures() {}
+
+// Reset Particles
+void Application::reset_particles() {
+	// Initializes the particles.
+	//particles.clear();
+
+	// Initialize random number generators
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> int_dist(0, 2);  // Random light id
+	std::uniform_real_distribution<> real_dist(0.0f, 5000.0f);  // Random delay in ms
+
+	// Zeroes the particle data.
+	for (int i = 0; i < current_particle_count; i++) {
+		// Initializes the particle.
+		Particle particle;
+
+		// Initializes the particle position.
+		particle.position = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+		particle.lifetime = real_dist(gen);
+
+		// Adds the particle to the list.
+		particles.push_back(particle);
+	}
+
+	// Updates the particle buffer.
+	update_particles_buffer();
+}
+
+// Update Particles Buffer
+void Application::update_particles_buffer() {
+	glBindBuffer(GL_SHADER_STORAGE_BUFFER, particle_buffer);
+	glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(Particle) * current_particle_count, particles.data(), GL_DYNAMIC_DRAW);
+	glBindBuffer(GL_SHADER_STORAGE_BUFFER, 0);
+
+	std::cout << "Size of Particle: " << sizeof(Particle) << std::endl;
+	std::cout << "Alignment of Particle: " << alignof(Particle) << std::endl;
+	std::cout << "Particles buffer updated." << std::endl;
+}
+
+// Update
+void Application::update(float delta) {
+	PV227Application::update(delta);
+
+	// Updates the main camera.
+	const glm::vec3 eye_position = camera.get_eye_position();
+	camera_ubo.set_view(lookAt(eye_position, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f)));
+	camera_ubo.update_opengl_data();
+
+	current_particle_count = desired_particle_count;
+
+	// Updates the global time delta.
+	t_delta = delta;
+}
+
+// Star Simulation (DISPLAY_STAR_PARTICLE)
+void Application::render_star_simulation() {
+	glDepthMask(GL_FALSE);
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_ONE, GL_ONE);
+	
+	star_particle_program.use();
+	star_particle_program.uniform("t_time", (float)elapsed_time);
+	star_particle_program.uniform("t_delta", (float)t_delta * 0.0001f);
+	star_particle_program.uniform("particle_size_vs", particle_size);
+
+	// Binds the particle texture.
+	glBindTextureUnit(0, star_tex);
+
+	// Binds the particle buffer.
+	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 3, particle_buffer);
+
+	// Renders the particles.
+	glBindVertexArray(empty_vao);
+	glDrawArrays(GL_POINTS, 0, current_particle_count);
+
+	glDepthMask(GL_TRUE);
+	glDisable(GL_BLEND);
+}
+
+// Render Scene
+void Application::render() {
+	// Starts measuring the elapsed time.
+	glBeginQuery(GL_TIME_ELAPSED, render_time_query);
+
+	// Binds the main window framebuffer.
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+	glViewport(0, 0, width, height);
+
+	// Clears the framebuffer color.
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+
+	// Binds the necessary buffers.
+	camera_ubo.bind_buffer_base(CameraUBO::DEFAULT_CAMERA_BINDING);
+
+	if (display_mode == DISPLAY_STAR_PARTICLE) {
+		render_star_simulation();
+	}
+
+	// Resets the VAO and the program.
+	glBindVertexArray(0);
+	glUseProgram(0);
+
+	// Stops measuring the elapsed time.
+	glEndQuery(GL_TIME_ELAPSED);
+
+	// Waits for OpenGL - don't forget OpenGL is asynchronous.
+	glFinish();
+
+	// Evaluates the query.
+	GLuint64 render_time;
+	glGetQueryObjectui64v(render_time_query, GL_QUERY_RESULT, &render_time);
+	fps_gpu = 1000.f / (static_cast<float>(render_time) * 1e-6f);
+}
+
+// GUI
+void Application::render_ui() {
+	const float unit = ImGui::GetFontSize();
+
+	ImGui::Begin("Controls", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+
+	std::string fps_cpu_string = "FPS (CPU): ";
+	ImGui::Text(fps_cpu_string.append(std::to_string(fps_cpu)).c_str());
+
+	std::string fps_string = "FPS (GPU): ";
+	ImGui::Text(fps_string.append(std::to_string(fps_gpu)).c_str());
+
+	if (ImGui::Combo("Display", &display_mode, DISPLAY_NAMES, IM_ARRAYSIZE(DISPLAY_NAMES))) {
+		reset_particles();
+	}
+
+	const char* particle_labels[10] = { "256", "512", "1024", "2048", "4096", "8192", "16384", "32768", "65536", "131072" };
+	int exponent = static_cast<int>(log2(current_particle_count) - 8);
+	if (ImGui::Combo("Particle Count", &exponent, particle_labels, IM_ARRAYSIZE(particle_labels))) {
+		desired_particle_count = static_cast<int>(glm::pow(2, exponent + 8));
+		update_particles_buffer();
+	}
+
+	ImGui::SliderFloat("Particle Size", &particle_size, 0.1f, 2.0f, "%.1f");
+
+	if (ImGui::Button("Reset Particles", ImVec2(150.f, 0.f))) {
+		reset_particles();
+	}
+
+	ImGui::End();
+}
+
+void Application::on_resize(int width, int height) {
+	PV227Application::on_resize(width, height);
+	resize_fullscreen_textures();
+}
---
+++ application.hpp
@@ -0,0 +1,122 @@
+#pragma once
+
+#include "camera_ubo.hpp"
+#include "light_ubo.hpp"
+#include "phong_material_ubo.hpp"
+#include "pv227_application.hpp"
+#include "ubo_impl.hpp"
+
+struct Particle {
+	glm::vec4 position; // The position of particles (on CPU).
+	glm::vec3 velocity; // The velocity of particles (on CPU).
+	float lifetime; // The lifetime of the particle
+	glm::vec3 color; // The colors of all particles (on GPU).
+	float remaining; // The remaining time of the particle
+};
+
+class Application : public PV227Application {
+	// Variables (Geometry)
+protected:
+	// Variables (Textures)
+protected:
+	GLuint star_tex;
+	// Variables (Light)
+protected:
+	// Variables (Camera)
+protected:
+	CameraUBO camera_ubo;
+	// Variables (Shaders)
+protected:
+	ShaderProgram star_particle_program;
+
+	// Variables (Frame Buffers)
+protected:
+	// Variables (GUI)
+protected:
+	// Variables (Particles)
+
+	// The desired number of particles.
+	int desired_particle_count = 4096;
+
+	// The current number of particles.
+	int current_particle_count = desired_particle_count;
+
+	// The maximum number of particles.
+	int max_particle_count = 131072;
+
+	// The particle size.
+	float particle_size = 0.1f;
+
+	// The particle data.
+	std::vector<Particle> particles;
+
+	// The particle buffer.
+	GLuint particle_buffer;
+
+protected:
+	/** The constants identifying what can be displayed on the screen. */
+	const int DISPLAY_STAR_PARTICLE = 0;
+	
+	const char* DISPLAY_NAMES[1] = { "Star Simulation" };
+
+	int display_mode = DISPLAY_STAR_PARTICLE;
+
+	/** Global Time Delta */
+	float t_delta = 0;
+
+public:
+	Application(int initial_width, int initial_height, std::vector<std::string> arguments = {});
+
+	/** Destroys the {@link Application} and releases the allocated resources. */
+	virtual ~Application();
+
+	// Shaders
+	void compile_shaders() override;
+public:
+	/** Prepares the required cameras. */
+	void prepare_cameras();
+
+	/** Prepares the required materials. */
+	void prepare_materials();
+
+	/** Prepares the required textures. */
+	void prepare_textures();
+
+	/** Prepares the lights. */
+	void prepare_lights();
+
+	/** Prepares the scene objects. */
+	void prepare_scene();
+
+	/** Prepares the frame buffer objects. */
+	void prepare_framebuffers();
+
+	/** Resizes the full screen textures match the window. */
+	void resize_fullscreen_textures();
+
+	/** Resets the particles */
+	void reset_particles();
+
+	/** Updates the particles on CPU */
+	void update_particles_buffer();
+
+	// Update
+	void update(float delta) override;
+
+	// Render Modes
+public:
+	/** Render Star Simulation (DISPLAY_STAR_PARTICLE) */
+	void render_star_simulation();
+
+public:
+	// Render
+	void render() override;
+
+public:
+	// Render UI
+	void render_ui() override;
+
+public:
+	// On Resize Callbak
+	void on_resize(int width, int height) override;
+};
